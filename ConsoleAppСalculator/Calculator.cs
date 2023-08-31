dd5c05a9a257cee3a92e29c24e8a5735e5f8fc65
Vadim <sedushkin27@gmail.com>
2023-08-31 11:52:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppСalculator/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCalculator
+{
+    internal class Calculator
+    {
+        public event EventHandler DivideByZero;
+
+        public double Add(double a, double b) => a + b;
+
+        public double Subtract(double a, double b) => a - b;
+     
+
+        public double Multiply(double a, double b) => a * b;
+     
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                OnDivideByZero();
+                return 0;
+            }
+
+            return a / b;
+        }
+
+        protected virtual void OnDivideByZero()
+        {
+            DivideByZero?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ ConsoleAppСalculator/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleAppCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.DivideByZero += HandleDivideByZero;
+
+            Console.WriteLine("Enter two numbers:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an operation (+, -, *, /):");
+            string operation = Console.ReadLine();
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = calculator.Add(a, b);
+                    break;
+                case "-":
+                    result = calculator.Subtract(a, b);
+                    break;
+                case "*":
+                    result = calculator.Multiply(a, b);
+                    break;
+                case "/":
+                    result = calculator.Divide(a, b);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+
+            Console.WriteLine($"Result: {result}");
+        }
+        static void HandleDivideByZero(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Cannot divide by zero!");
+            Console.ResetColor();
+        }
+    }
+
+}
